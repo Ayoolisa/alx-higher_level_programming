114269f526fb4b9390b94805a0a9b12d1a9b20cb
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-12-02 06:56:46 -0800

    Create 2-print_alphabet.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 5:
-    print(f"Last digit of {number} is {number} and is greater than 5")
-elif number == 0:
-    print(f"Last digit of {number} is {number} and is 0")
-elif number < 6 and number != 0:
-    print(f"Last digit of {number} is {number} and is less than 6 and is not 0")
+last_digit = number % 10
+
+if number < 0:
+    last_digit = ((number * - 1) % 10) * -1
+
+str_first_part = f"Last digit of {number:d} is {last_digit:d}"
+
+if last_digit > 5:
+    print(f"{str_first_part} and is greater than 5")
+if last_digit == 0:
+    print(f"{str_first_part} and is 0")
+if last_digit < 6 and last_digit != 0:
+    print(f"{str_first_part} and is less than 6 and not 0")
