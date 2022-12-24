f3d732652ce16bdb2aa523e770c4a0d5659482df
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-12-23 18:19:20 -0800

    Create 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """
-	    a function that replaces all 
-	    occurrences of an element by 
-	    another in a new list.
+        a function that replaces all
+        occurrences of an element by
+        another in a new list.
 
-	    my_list - the initial list
-	    search - the element to be replaced
-	    replace - the new element
+        my_list - the initial list
+        search - the element to be replaced
+        replace - the new element
     """
     new_list = my_list[:]
     for i in range(len(new_list)):
