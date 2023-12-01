25ef3326693578e07d59e62e7f4b1bb2f61d206b
Ayomide Olisa <lekelisa247@gmail.com>
2023-12-01 04:36:12 -0800

    update files

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
-import requests
-
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    import requests
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    body = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
